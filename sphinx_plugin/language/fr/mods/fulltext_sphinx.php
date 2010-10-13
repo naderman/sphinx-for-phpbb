fe711212499f1c5dd23443e2a954183a7cfde366
maelsoucaze <maelsoucaze@gmail.com>
2010-10-13 02:57:25 +0200

    Changing French localization copyright.

--- sphinx_plugin/language/fr/mods/fulltext_sphinx.php
+++ sphinx_plugin/language/fr/mods/fulltext_sphinx.php
@@ -2,7 +2,7 @@
 /**
 *
 * This file is part of French (Formal Honorifics) Sphinx-for-phpBB translation.
-* Copyright (C) 2010 phpBB.fr
+* Copyright (C) 2010 Maël Soucaze
 *
 * This program is free software; you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -20,7 +20,7 @@
 * fulltext_sphinx [French (Formal Honorifics)]
 *
 * @package   language
-* @author    Maël Soucaze <maelsoucaze@phpbb.fr> (Maël Soucaze) http://www.phpbb.fr/
+* @author    Maël Soucaze <maelsoucaze@gmail.com> http://mael.soucaze.com/
 * @copyright (c) 2005 phpBB Group 
 * @license   http://www.gnu.org/licenses/gpl-2.0.html GNU General Public License
 * @version   $Id$ 
